49bd7ce96b3c079a8d9ba4d0b159b0f720d06d50
artemMurygin <artem@murygin.tech>
2026-02-21 16:42:47 +0300

    hot fix

--- client/src/pages/CreateBook/CreateBook.tsx
+++ client/src/pages/CreateBook/CreateBook.tsx
@@ -7,6 +7,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useState } from 'react'
 
+
 type CreateBookForm = {
     title: string
     description: string
@@ -40,7 +41,7 @@ function CreateBook() {
                 description: formData.description.trim() || null,
                 publish_year: formData.publish_year.trim() ? Number(formData.publish_year) : null,
                 cover_url: formData.cover_url.trim() || null,
-                status: 'DRAFT'
+                status: 'PUBLISHED'
             }
 
             const { data } = await $api.post<CreatedBook>('/books', payload)
